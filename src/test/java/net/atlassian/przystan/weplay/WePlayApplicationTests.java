26c2cab9323f51712d5f0d134ade07af3fdff2b0
luckybard <bartosz.mucha@outlook.com>
2023-06-17 00:58:35 +0200

    feat: add spring starter

---
+++ src/test/java/net/atlassian/przystan/weplay/WePlayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atlassian.przystan.weplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WePlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
